e3bd199356be339b0755b7af76966cbb96d6c95d
deepagavhande <deepagavhande21@gmail.com>
2021-05-10 17:10:40 +0530

    1st commit

---
+++ src/main/java/com/cg/gitPractice/javaProgram.java
@@ -0,0 +1,17 @@
+package com.cg.gitPractice;
+
+public class javaProgram {
+	
+
+public static void main(String[] args) {
+		
+		String[] argh={"A","B"};
+		
+		args=argh;
+		
+		for(int i=0;i<args.length;i++){
+			
+			System.out.println(args[i]);
+		}
+}
+}
